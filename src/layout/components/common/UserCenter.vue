da5c8de2e419bf6bad257c491c6eed71a3553ecd
chocho <849890769@qq.com>
2025-08-25 17:06:51 +0800

    feat:layout调整
    用户中心组件化

---
+++ src/layout/components/common/UserCenter.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-dropdown trigger="click">
+    <div class="avatar-container">
+      <img-load :src="ctx.userInfo.avatar" defaultName="user" backgroundColor="rgba(0, 0, 0, .05)" :defaultColor="variables.menuActiveText" defaultWidth="50%" defaultHeight="50%" />
+    </div>
+    <template v-slot:dropdown>
+      <el-dropdown-menu>
+        <router-link to="/">
+          <el-dropdown-item>Home</el-dropdown-item>
+        </router-link>
+        <a href="https://github.com/chocho-1115/vue-admin" target="_blank">
+          <el-dropdown-item>Github</el-dropdown-item>
+        </a>
+        <el-dropdown-item @click.native="onLogout" divided>
+          <span style="display:block;">Log Out</span>
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+
+<script setup>
+import { inject } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+
+import { logout } from '@/api/user'
+import { dispatch } from '@/store'
+import variables from '@/styles/variables.module.scss'
+
+const ctx = inject('context')
+
+const router = useRouter()
+const route = useRoute()
+
+const redirect = route.fullPath || '/'
+
+const onLogout = async () => {
+  logout().then(() => {
+    dispatch.user.removeInfo()
+    router.push(`/account/login?redirect=${redirect}`)
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+.avatar-container {
+  cursor: pointer;
+  display: block;
+  width: 40px;
+  height: 40px;
+  border-radius: 10px;
+  overflow: hidden;
+}
+</style>
