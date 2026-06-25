ddd29956505aed087b9780c9244ff8922b4e5450
chocho <849890769@qq.com>
2026-06-25 17:24:45 +0800

    Update login.vue

--- src/views/account/login.vue
+++ src/views/account/login.vue
@@ -109,60 +109,12 @@ const handleLogin = () => {
 }
 </script>
 
-<style lang="scss">
-/* 修复input 背景不协调 和光标变色 */
-/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
-
-$bg: #283443;
-$light_gray: #fff;
-$cursor: #fff;
-
-@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
-	.login-container .el-input input {
-		color: $cursor;
-	}
-}
-
-/* reset element-ui css */
-.login-container {
-	.el-input {
-		.el-input__wrapper {
-			background: transparent;
-			box-shadow: none;
-		}
-
-		input {
-			-webkit-appearance: none;
-			color: $light_gray;
-			caret-color: $cursor;
-
-			&:-webkit-autofill {
-				box-shadow: 0 0 0px 1000px transparent inset !important;
-				-webkit-text-fill-color: $cursor !important;
-				transition: background-color 50000s ease-in-out 0s; //背景色透明 生效时长 过渡效果 启用时延迟的时间
-			}
-		}
-	}
-
-	.el-form-item {
-		border: 1px solid rgba(255, 255, 255, 0.1);
-		background: rgba(0, 0, 0, 0.1);
-		border-radius: 5px;
-	}
-}
-</style>
-
 <style lang="scss" scoped>
-$bg: #2d3a4b;
-$dark_gray: #889aa4;
-$light_gray: #eee;
-
 .login-container {
 	min-height: 100vh;
 	width: 100%;
-	background-color: $bg;
+	background-color: var(--el-bg-color);
 	overflow: hidden;
-
 	.login-form {
 		position: relative;
 		width: 520px;
@@ -171,10 +123,9 @@ $light_gray: #eee;
 		margin: 0 auto;
 		overflow: hidden;
 	}
-
 	.tips {
 		font-size: 14px;
-		color: #fff;
+		color: var(--el-text-color-regular);
 		margin-bottom: 10px;
 
 		span {
@@ -183,13 +134,11 @@ $light_gray: #eee;
 			}
 		}
 	}
-
 	.title-container {
 		position: relative;
-
 		.title {
 			font-size: 26px;
-			color: $light_gray;
+			color: var(--el-text-color-primary);
 			margin: 0px auto 40px auto;
 			text-align: center;
 			font-weight: bold;
