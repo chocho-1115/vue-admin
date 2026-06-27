598d72e69fe20db1afef807f83cb86db47014bee
chocho <849890769@qq.com>
2026-06-27 20:06:15 +0800

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -24,4 +24,10 @@ export default defineConfig([
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   skipFormatting,
+
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off', // 关闭vue组件名称必须多单词
+    },
+  },
 ])
