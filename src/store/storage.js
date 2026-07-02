cc84cb59c8cfd9d86a6211862ab9eec11547b5c7
chocho <849890769@qq.com>
2026-07-02 09:08:30 +0800

    Update storage.js

--- src/store/storage.js
+++ src/store/storage.js
@@ -1,6 +1,6 @@
-const sidebarOpenedCacheKey = 'VA_mini-sidebar'
 const tokenCacheKey = 'VA_token'
 const themeCacheKey = 'VA_theme'
+const sidebarOpenedCacheKey = 'VA_mini-sidebar'
 
 export function getTokenStorage() {
   return localStorage.getItem(tokenCacheKey)
@@ -14,6 +14,14 @@ export function removeTokenStorage() {
   return localStorage.removeItem(tokenCacheKey)
 }
 
+export function setTheme(tName) {
+  localStorage.setItem(themeCacheKey, tName)
+}
+
+export function getTheme() {
+  return localStorage.getItem(themeCacheKey) || ''
+}
+
 export function getSidebarStatus() {
   const status = localStorage.getItem(sidebarOpenedCacheKey)
   return status ? !!+status : true
@@ -23,10 +31,4 @@ export function setSidebarStatus(status) {
   localStorage.setItem(sidebarOpenedCacheKey, status ? 1 : 0)
 }
 
-export function setTheme(tName) {
-  localStorage.setItem(themeCacheKey, tName)
-}
 
-export function getTheme() {
-  return localStorage.getItem(themeCacheKey) || ''
-}
