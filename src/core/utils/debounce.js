6cdc9879298b6f18a9ac9cc16f721c52f195b2e0
chocho <849890769@qq.com>
2026-07-14 15:19:41 +0800

    lint

--- src/core/utils/debounce.js
+++ src/core/utils/debounce.js
@@ -1,14 +1,12 @@
 // 函数防抖 多次触发只执行最后一次  此函数不执行第一次触发
 function debounce(method, delay) {
-  let timeout
-  return function () {
-    let context = this // 保存this指向
-    let args = arguments // 拿到event对象
-    clearTimeout(timeout)
-    timeout = setTimeout(function () {
-      method.apply(context, args)
-    }, delay)
-  }
+	let timeout
+	return function (...args) {
+		clearTimeout(timeout)
+		timeout = setTimeout(() => {
+			method.apply(this, args)
+		}, delay)
+	}
 }
 
 export default debounce
--- src/core/utils/throttle.js
+++ src/core/utils/throttle.js
@@ -1,22 +1,20 @@
 // 函数节流 多次触发时减少触发频次 此函数会执行第一次触发
 function throttle(method, delay) {
-  let timer = null
-  let starttime = Date.now()
-  return function () {
-    let curTime = Date.now() // 当前时间
-    let remaining = delay - (curTime - starttime) // 从上一次到现在，还剩下多少多余时间
-    let context = this
-    let args = arguments
-    clearTimeout(timer)
-    if (remaining <= 0) {
-      method.apply(context, args)
-      starttime = Date.now()
-    } else {
-      timer = setTimeout(function () {
-        method.apply(context, args)
-      }, remaining)
-    }
-  }
+	let timer = null
+	let starttime = Date.now()
+	return function (...args) {
+		const curTime = Date.now() // 当前时间
+		const remaining = delay - (curTime - starttime) // 从上一次到现在，还剩下多少多余时间
+		clearTimeout(timer)
+		if (remaining <= 0) {
+			method.apply(this, args)
+			starttime = Date.now()
+		} else {
+			timer = setTimeout(() => {
+				method.apply(this, args)
+			}, remaining)
+		}
+	}
 }
 
 export default throttle
